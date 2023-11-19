23f3c1c5eb48d155b7b2ea8339f25216f6b92ec5
Rustafarian Dev <rustafarian.dev@gmail.com>
2023-11-19 20:04:21 +0000

    importing frameworks into it's own file

---
+++ cmd/frameworks.go
@@ -0,0 +1,13 @@
+package cmd
+
+import (
+	_ "github.com/melkeydev/go-blueprint/cmd/frameworks/caddy"
+	_ "github.com/melkeydev/go-blueprint/cmd/frameworks/chi"
+	_ "github.com/melkeydev/go-blueprint/cmd/frameworks/cobra"
+	_ "github.com/melkeydev/go-blueprint/cmd/frameworks/echo"
+	_ "github.com/melkeydev/go-blueprint/cmd/frameworks/fiber"
+	_ "github.com/melkeydev/go-blueprint/cmd/frameworks/gin"
+	_ "github.com/melkeydev/go-blueprint/cmd/frameworks/gorilla"
+	_ "github.com/melkeydev/go-blueprint/cmd/frameworks/httprouter"
+	_ "github.com/melkeydev/go-blueprint/cmd/frameworks/standard-library"
+)
