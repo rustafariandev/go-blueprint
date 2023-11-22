38f2a964df1e42e7e7641eae11f5051d88212af1
Rustafarian Dev <rustafarian.dev@gmail.com>
2023-11-22 20:27:21 +0000

    Use the common files for all the frameworks

--- cmd/frameworks/cobra/fs.go
+++ cmd/frameworks/cobra/fs.go
@@ -3,14 +3,16 @@ package cobra
 import (
 	"embed"
 
+	"github.com/melkeydev/go-blueprint/cmd/frameworks/common"
 	"github.com/melkeydev/go-blueprint/cmd/registry"
+	"github.com/yalue/merged_fs"
 )
 
 //go:embed blueprint.toml cmd all:*.tmpl
 var fs embed.FS
 
 func init() {
-	err := registry.RegisterProviderFromFS(fs)
+	err := registry.RegisterProviderFromFS(merged_fs.NewMergedFS(fs, common.FS))
 	if err != nil {
 		panic(err.Error())
 	}
--- cmd/frameworks/gorilla/fs.go
+++ cmd/frameworks/gorilla/fs.go
@@ -3,14 +3,16 @@ package gorilla
 import (
 	"embed"
 
+	"github.com/melkeydev/go-blueprint/cmd/frameworks/common"
 	"github.com/melkeydev/go-blueprint/cmd/registry"
+	"github.com/yalue/merged_fs"
 )
 
-//go:embed blueprint.toml internal cmd all:*.tmpl
+//go:embed blueprint.toml internal cmd
 var fs embed.FS
 
 func init() {
-	err := registry.RegisterProviderFromFS(fs)
+	err := registry.RegisterProviderFromFS(merged_fs.NewMergedFS(fs, common.FS))
 	if err != nil {
 		panic(err.Error())
 	}
